a4630f3450370781ca9b4aff004c6b55426f0dfd
puru1joy <puru1joy@gmail.com>
2016-03-03 16:08:30 +0530

    reduced the number of writes on the cells

--- dist/js/main.js
+++ dist/js/main.js
@@ -3613,14 +3613,15 @@
 
 //TODO right this in a module style
 
-console.log(moment('29-2-2016', 'DD-MM-YYYY').isValid());
+console.log();
 
 initDialog(moment());
 
 //to initiate the date picker dialog
 function initDialog(date) {
   // console.log(moment(date).format('YYYY'));
-  var m = moment(date);
+  //TODO REPLACE [2016, 2, 1] with date
+  var m = moment([2016, 2, 1]);
   var pickerDialog = '.md-picker-date ';
   var current = '.md-picker__view--current ';
   var previous = '.md-picker__view--previous ';
@@ -3628,12 +3629,51 @@ function initDialog(date) {
   document.querySelector(pickerDialog).setAttribute('data-date', moment(date));
   document.querySelector(pickerDialog + '.md-picker__subtitle').innerHTML = m.format('YYYY');
   document.querySelector(pickerDialog + '.md-picker__title').innerHTML = m.format('ddd, MMM D');
-  //set evrything for the current month
-  document.querySelector(pickerDialog + current + '.md-picker__month').innerHTML = m.format('MMMM YYYY');
+  //set everything for the current month
+  initMonth(pickerDialog + current, m);
   //set everything for the previous month
   document.querySelector(pickerDialog + previous + '.md-picker__month').innerHTML = getPreviousMonthString(m);
   //set everything for the next month
   document.querySelector(pickerDialog + next + '.md-picker__month').innerHTML = getNextMonthString(m);
+}
+
+//init month
+function initMonth(selector, m) {
+  //set month title
+  document.querySelector(selector + '.md-picker__month').innerHTML = m.format('MMMM YYYY');
+  var cells = document.querySelectorAll(selector + '.md-picker__tr ' + 'span');
+  //calculate prerequisites
+  var firstDayOfMonth = parseInt(moment(m).date(1).day());
+  var today = -1;
+  var selected = -1;
+  var lastDayoFMonth = parseInt(moment(m).endOf('month').format('D')) + firstDayOfMonth - 1;
+  //if this month and year is the same as the today's month and year respectively
+  if (!(moment().diff(m, 'month')) && !(moment().diff(m, 'year'))) {
+    today = parseInt(moment().format('D'));
+    today += firstDayOfMonth - 1;
+    selected = parseInt(moment(m).format('D'));
+    selected += firstDayOfMonth - 1;
+  }
+  console.log(lastDayoFMonth);
+  for (var i in cells) {
+    var cell = cells[i];
+    //if the cell is before this month's first date
+    if (i < firstDayOfMonth) {
+      cell.classList.remove('md-picker__cell');
+    }
+    //if today add the today class
+    if (today == i) {
+      cell.classList.add('md-picker__today');
+    }
+    //if the selected date is here
+    if (selected == i) {
+      cell.classList.add('md-picker__selected');
+    }
+    //if the cell is after this month's last date
+    if (i > lastDayoFMonth) {
+      cell.classList.remove('md-picker__cell');
+    }
+  }
 }
 
 //helper functions for general calculations
