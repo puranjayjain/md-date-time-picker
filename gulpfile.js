421950a3d74650e1ae4801d6632490c5d4c39284
puru1joy <puru1joy@gmail.com>
2016-03-13 13:36:35 +0530

    added nunjucks templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync');
 
 //process sass
 gulp.task('sass', function ()
@@ -63,4 +68,84 @@ gulp.task('copyFolders', function ()
 gulp.task('distributable', function ()
 {
 	gulp.watch('src/*.php', ['copyFolders','minifyCss','minifyJs','copyROOT']);
+});
+
+var src = {
+	scss: 'src/scss/**/*.scss',
+	images: 'src/images/**/*',
+	js: 'src/js/**/*.js',
+	svg: 'src/images/**/*.svg'
+};
+
+var dist = {
+	css: 'dist/css/',
+	html: 'dist/*.html',
+	images: 'dist/images/',
+	js: 'dist/js/'
+};
+
+gulp.task('browser-sync', function () {
+	browserSync({
+		server: {
+			baseDir: "./dist"
+		}
+	});
+});
+
+gulp.task('bs-reload', function () {
+	browserSync.reload();
+});
+
+gulp.task('images', function () {
+	gulp.src(src.images)
+		.pipe(cache(imagemin({
+			optimizationLevel: 3,
+			progressive: true,
+			interlaced: true
+		})))
+		.pipe(gulp.dest(dist.images));
+});
+
+gulp.task('styles', function () {
+	gulp.src([src.scss])
+		.pipe(plumber({
+			errorHandler: function (error) {
+				console.log(error.message);
+				this.emit('end');
+			}
+		}))
+		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 4 versions', 'ie >= 6']
+		}))
+		.pipe(gulp.dest(dist.css))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(minifycss())
+		.pipe(gulp.dest(dist.css))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task('scripts', function () {
+	return gulp.src(src.js)
+		.pipe(babel())
+		.pipe(gulp.dest(dist.js))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest(dist.js))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task('default', ['browser-sync'], function () {
+	gulp.watch(src.scss, ['styles']);
+	gulp.watch(src.js, ['scripts']);
+	gulp.watch(src.svg, ['images']);
+	gulp.watch(dist.html, ['bs-reload']);
 });
