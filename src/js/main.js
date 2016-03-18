3b87accb69a5faa182cab161433282b9fe36f73b
puru1joy <puru1joy@gmail.com>
2016-03-18 23:08:18 +0530

    added code highlighting

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,9 @@
-// highlight js code block
-hljs.initHighlightingOnLoad();
+(function() {
+  // highlight js code block
+  hljs.initHighlightingOnLoad();
+  // example on page 1 script
+  var x = new mdDateTimePicker('date');
+  document.getElementById('trigger-1').addEventListener('click', function() {
+    x.toggle();
+  });
+}).call(this);
