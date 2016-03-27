36d6808e7b767e6cc7a74e0aa0df9fab17efc2be
puru1joy <puru1joy@gmail.com>
2016-03-27 12:51:28 +0530

    fixed documentation sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,15 @@
   document.getElementById('trigger-1').addEventListener('click', function() {
     x.toggle();
   });
+  var header = document.getElementById('mddtpp-header');
+  var documentation = document.getElementById('documentation/toc')
+  document.getElementById('mddtpp-layout').addEventListener('scroll', function () {
+    var scrollBarPosition = this.scrollTop;
+    var threshold = header.clientHeight;
+    if (scrollBarPosition > threshold) {
+      documentation.setAttribute('style', 'margin-top: ' + (scrollBarPosition - threshold) + 'px');
+    } else {
+      documentation.removeAttribute('style');
+    }
+  });
 }).call(this);
