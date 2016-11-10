e7e95d8b5716e001bdc3b9194fb00a922459adc6
mh-cbon <mh-cbon@users.noreply.github.com>
2016-11-10 13:43:16 +0100

    gulp: remove useless var

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var config = require('../config');
 var browserSync = require('browser-sync');
 
-gulp.task('default', ['browserSyncTask'], function (a, b) {
+gulp.task('default', ['browserSyncTask'], function () {
   	gulp.watch(config.src.js, ['scripts', browserSync.reload]);
   	gulp.watch(config.src.scss, ['styles', browserSync.reload]);
   	gulp.watch(config.src.svg, ['images', browserSync.reload]);
