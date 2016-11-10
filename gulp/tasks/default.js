7b0072b214e05c7781abaa1c912ace5b7a468d63
mh-cbon <mh-cbon@users.noreply.github.com>
2016-11-10 23:01:11 +0100

    fix watched html files source

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -6,5 +6,5 @@ gulp.task('default', ['browserSyncTask'], function () {
   	gulp.watch(config.src.js, ['scripts', browserSync.reload]);
   	gulp.watch(config.src.scss, ['styles', browserSync.reload]);
   	gulp.watch(config.src.svg, ['images', browserSync.reload]);
-  	gulp.watch(config.src.html, ['bsReload', browserSync.reload]);
+  	gulp.watch(config.dist.html, ['bsReload', browserSync.reload]);
 });
