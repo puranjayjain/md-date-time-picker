588a2b0c12b696117291b678cb7630318b251326
mh-cbon <mh-cbon@users.noreply.github.com>
2016-11-10 14:06:24 +0100

    fix indentation

--- src/js/mdDateTimePicker.js
+++ src/js/mdDateTimePicker.js
@@ -22,7 +22,7 @@ class mdDateTimePicker {
 	* @param  {moment}   init 					[initial value for the dialog date or time, defaults to today] [@default = today]
 	* @param  {moment}   past 					[the past moment till which the calendar shall render] [@default = exactly 21 Years ago from init]
 	* @param  {moment}   future	 				[the future moment till which the calendar shall render] [@default = init]
-	* @param	{Boolean}  mode 				[this value tells whether the time dialog will have the 24 hour mode (true) or 12 hour mode (false)] [@default = false]
+	* @param  {Boolean}  mode 					[this value tells whether the time dialog will have the 24 hour mode (true) or 12 hour mode (false)] [@default = false]
 	* @param  {String}   orientation = 'LANDSCAPE' or 'PORTRAIT'	[force the orientation of the picker @default = 'LANDSCAPE']
 	* @param  {element}  trigger					[element on which all the events will be dispatched e.g var foo = document.getElementById('bar'), here element = foo]
 	* @param  {String}  ok = 'ok'					[ok button's text]
